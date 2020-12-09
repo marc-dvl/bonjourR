6a367727e6b4cb427d3046b72938e9e8be20b9d2
marc-dvl <marcelie.dersoir@gmail.com>
2020-12-09 11:52:21 +0100

    ajouté un print successif du nom de VGE

--- bonjour_2.r
+++ bonjour_2.r
@@ -1,3 +1,5 @@
 ##mon prmier projer R pour essayer les fonctions de git
 print("Bonjour, monde")
-print("rebonjour, monde")
+print("au revoir monde")
+nom_président <- c('Valéry','Giscard',"d'Estaing")
+for(i in nom_président) print(i)
